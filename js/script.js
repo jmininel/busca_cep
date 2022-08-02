293d66ca620279f133051255b397ac271c615eac
jmininel <ju.mininel@hotmail.com>
2022-08-02 19:51:30 -0300

    created script js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const inputCepEl = document.querySelector('#inputCep');
+const inputLogrEl = document.querySelector('#inputLogr');
+const inputBairroEl = document.querySelector('#inputBairro');
+const inputNumeroEl = document.querySelector('#inputNumero');
+const inputComplEl = document.querySelector('#inputCompl');
+const inputCidadeEl = document.querySelector('#inputCidade');
+const inputEstadoEl = document.querySelector('#inputEstado')
+
+const pegar = inputCepEl.value
+
+console.log(pegar)
+
+
