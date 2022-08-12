1037c10fc6f52d7f03cfadd1d00c6b0e32a6e9b2
jmininel <ju.mininel@hotmail.com>
2022-08-12 15:47:19 -0300

    update Css and Js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
-const inputCepEl = document.querySelector('#inputCep');
-const inputLogrEl = document.querySelector('#inputLogr');
-const inputBairroEl = document.querySelector('#inputBairro');
-const inputNumeroEl = document.querySelector('#inputNumero');
-const inputComplEl = document.querySelector('#inputCompl');
-const inputCidadeEl = document.querySelector('#inputCidade');
-const inputEstadoEl = document.querySelector('#inputEstado')
-
-const pegar = inputCepEl.value
-
-console.log(pegar)
+const cep = document.querySelector("#inputCep")
+const rua = document.querySelector("#inputLogr")
+const bairro = document.querySelector("#inputBairro")
+const cidade = document.querySelector("#inputCidade")
+const estado = document.querySelector("#inputEstado")
 
 
+cep.addEventListener('click', (e) => {
+  const pegaInputCep = e.target.value
+
+  getApi(pegaInputCep)
+})
+
+const getApi = async(cep) => {
+  const api = `https://viacep.com.br/ws/${cep}/json/`
+
+  const response = await fetch(api);
+  const data = await response.json();
+
+  console.log(data)
+  rua.value = data.logradouro
+  bairro.value = data.bairro
+  cidade.value = data.localidade
+  estado.value = data.uf
+}
